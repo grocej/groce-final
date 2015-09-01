a46326302917db288da8ede1d2e4ffa1c55efe25
grocej <grocej@gmail.com>
2015-09-01 12:14:16 -0400

    editing some html

--- app.js
+++ app.js
@@ -19,11 +19,3 @@
     })
 
 }());
-// $(document).ready(function() {
-//   $('.lyric-container').on('click', '#play', function(event) {
-//     event.preventDefault();
-//     console.log('grabbing');
-//     $('bowieList1[songsArr[randomBowie]].lyric').fadeIn(300);
-//   });
-// });
-// bowieList1[songsArr[randomBowie]].lyric
